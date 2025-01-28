40780e31e8463d2069f534837ab393744536a54b
marinos <marinos.agounkpe@epitech.eu>
2025-01-28 09:19:39 +0100

    configf profil Moi

--- frontEnd/src/app/home/components/header/header.component.ts
+++ frontEnd/src/app/home/components/header/header.component.ts
@@ -1,9 +1,45 @@
 
+// import { Component } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { IonicModule } from '@ionic/angular';
+// import { PopoverController } from '@ionic/angular';
+// import { PopoverComponent } from './popover/popover.component';
+
+// @Component({
+//   selector: 'app-header',
+//   templateUrl: './header.component.html',
+//   styleUrls: ['./header.component.scss'],
+//   standalone: true,
+//   imports: [
+//     CommonModule,
+//     IonicModule // Ajout des modules nécessaires pour utiliser les balises ioniques
+//   ]
+// })
+// export class HeaderComponent {
+//   constructor(private popoverController: PopoverController) {}
+
+//   async presentPopover(event: Event) {
+//          const popover = await this.popoverController.create({
+//           component: PopoverComponent, 
+//           cssClass: 'my-custom-class',
+//           event: event,
+//           showBackdrop: false,
+//         });
+//         await popover.present();
+
+//          const { role } = await popover.onDidDismiss();
+//          console.log('onDidDismiss resolved with role', role)
+//       }
+// }
+
+
+
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { PopoverController } from '@ionic/angular';
 import { PopoverComponent } from './popover/popover.component';
+
 
 @Component({
   selector: 'app-header',
@@ -12,23 +48,24 @@ import { PopoverComponent } from './popover/popover.component';
   standalone: true,
   imports: [
     CommonModule,
-    IonicModule // Ajout des modules nécessaires pour utiliser les balises ioniques
+    IonicModule,
+    
   ]
 })
 export class HeaderComponent {
   constructor(private popoverController: PopoverController) {}
 
   async presentPopover(event: Event) {
-         const popover = await this.popoverController.create({
-          component: PopoverComponent, // Remplacez par le composant à afficher dans le popover
-          event: event,
-           translucent: true
-        });
-        await popover.present();
+    const popover = await this.popoverController.create({
+      component: PopoverComponent, 
+      cssClass: 'my-custom-class',
+      event: event,
+      showBackdrop: false,
+    });
+    await popover.present();
 
-        // const { role } = await popover.onDidDismiss();
-        // console.log('onDidDismiss resolved with role', role)
-      }
+    const { role } = await popover.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
 }
-
 
--- frontEnd/src/app/home/components/header/popover/popover.component.ts
+++ frontEnd/src/app/home/components/header/popover/popover.component.ts
@@ -1,9 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+
 @Component({
   selector: 'app-popover',
   templateUrl: './popover.component.html',
   styleUrls: ['./popover.component.scss'],
+  standalone: true,
+  imports : [
+    CommonModule,
+    IonicModule
+  ]
 })
 export class PopoverComponent  implements OnInit {
 
@@ -12,3 +20,17 @@ export class PopoverComponent  implements OnInit {
   ngOnInit() {}
 
 }
+
+
+
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { PopoverComponent } from './popover.component';
+
+// @NgModule({
+//   declarations: [PopoverComponent],
+//   imports: [CommonModule],
+//   exports: [PopoverComponent], // Rendre disponible ailleurs
+// })
+// export class PopoverModule {}
+
