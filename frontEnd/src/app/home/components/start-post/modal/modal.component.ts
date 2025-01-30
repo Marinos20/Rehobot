c286fce4e9436ea6d45714215f231c294a16fec7
marinos <marinos.agounkpe@epitech.eu>
2025-01-30 10:36:49 +0100

    datapersiste

--- frontEnd/src/app/home/components/start-post/modal/modal.component.ts
+++ frontEnd/src/app/home/components/start-post/modal/modal.component.ts
@@ -1,23 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms'; // ✅ Importer FormsModule pour ngModel
+import { AfterViewInit, Component, OnInit, ViewChild, } from '@angular/core';
+import { IonicModule, ModalController } from '@ionic/angular';
+import { FormsModule, NgForm } from '@angular/forms'; 
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
   standalone: true,
-  imports: [IonicModule, FormsModule], // ✅ Ajouter FormsModule ici
+  imports: [IonicModule, FormsModule], 
 })
 export class ModalComponent implements OnInit {
-  postContent: string = ''; // ✅ Variable liée au ngModel
+  @ViewChild('form') form!: NgForm
 
-  constructor() {}
+
+  constructor(public modalController: ModalController) {}
 
   ngOnInit() {}
 
+  onDismiss() {
+    this.modalController.dismiss(null , 'dismiss')
+
+  }
+
   onPost() {
-    console.log('Post content:', this.postContent);
+    if (!this.form.valid) return;
+    const body = this.form.value['body'];
+    this.modalController.dismiss(
+      {
+        post : {
+          body,
+          createdAt: new Date(),
+        }
+      },
+      'post'
+    )
   }
 }
+
 
