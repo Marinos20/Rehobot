ad334b98ebbaf480cca2d39b91dae51df2e3fba8
Marinos <marinos.agounkpe@epitech.eu>
2025-02-15 22:41:18 +0100

    mise à jours

--- backend/src/auth/controllers/user.controller.ts
+++ backend/src/auth/controllers/user.controller.ts
@@ -1,22 +1,22 @@
-import { Controller, Post, UploadedFile, UseGuards, UseInterceptors, Request, BadRequestException, Logger, Res, Get } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseGuards, UseInterceptors, Request, BadRequestException, Logger, Res, Get, Param } from '@nestjs/common';
 import { JwtGuard } from '../guards/jwt.guard';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { isFileExtensionSafe, saveImageToStorage, removeFile } from '../helpers/image-storage';
 import { UserService } from '../services/user.service';
 import { switchMap, catchError, of, Observable } from 'rxjs';
 import { join } from 'path';
-import { promises as fs } from 'fs';
+import { existsSync } from 'fs';
 
 @Controller('user')
 export class UserController {
-    private readonly logger = new Logger(UserController.name);  // Logger pour afficher l'erreur dans les logs du serveur
+    private readonly logger = new Logger(UserController.name);
 
     constructor(private userService: UserService) {}
 
     @UseGuards(JwtGuard)
     @Post('upload')
     @UseInterceptors(FileInterceptor('file', saveImageToStorage))
-    uploadImage(@UploadedFile() file: Express.Multer.File, @Request() req): Observable<{ modifiedFileName: string }> {
+    uploadImage(@UploadedFile() file: Express.Multer.File, @Request() req): Observable<{ avatarUrl: string }> {
         if (!file) {
             throw new BadRequestException('Aucun fichier téléchargé ou fichier invalide. Veuillez télécharger une image.');
         }
@@ -24,29 +24,24 @@ export class UserController {
         const userId = req.user.id;
         const fullImagePath = join(process.cwd(), 'images', file.filename);
 
-        // Validation du fichier avant de le sauvegarder
         return isFileExtensionSafe(fullImagePath).pipe(
             switchMap((isFileLegit: boolean) => {
                 if (isFileLegit) {
                     return this.userService.updateUserImageById(userId, fullImagePath).pipe(
                         switchMap(() => {
-                            // Retourne l'URL de l'image modifiée
-                            const avatarUrl = `/user/image/${file.filename}`;  // URL relative pour accéder à l'image
-                            return of({ modifiedFileName: avatarUrl });
+                            const avatarUrl = `/user/image/${file.filename}`;
+                            return of({ avatarUrl });
                         }),
                     );
                 } else {
                     removeFile(fullImagePath);
-                    const errorMsg = `ERROR [ExceptionsHandler] Type de fichier non autorisé. Types valides: image/jpeg, image/jpg, image/png`;
-                    this.logger.error(errorMsg);  // Affiche l'erreur dans les logs
-                    throw new BadRequestException(errorMsg);  // Renvoie un message d'erreur
+                    this.logger.error(`Type de fichier non autorisé. Types valides: image/jpeg, image/jpg, image/png`);
+                    throw new BadRequestException('Type de fichier non autorisé.');
                 }
             }),
             catchError((err) => {
                 removeFile(fullImagePath);
-                const errorMsg = `ERROR [ExceptionsHandler] Erreur lors de la validation du fichier: ${err.message}`;
-                // Log l'erreur générale
-                this.logger.error(errorMsg);
+                this.logger.error(`Erreur lors de la validation du fichier: ${err.message}`);
                 throw new BadRequestException(`Erreur lors de la validation du fichier: ${err.message}`);
             })
         );
@@ -54,29 +49,20 @@ export class UserController {
 
     @UseGuards(JwtGuard)
     @Get('image/:imageName')
-    findImage(@Request() req, @Res() res): Observable<Object> {
-        const { imageName } = req.params;
+    findImage(@Param('imageName') imageName: string, @Res() res): void {
         const imagePath = join(process.cwd(), 'images', imageName);
 
-        return this.checkFileExists(imagePath).pipe(
-            switchMap((exists: boolean) => {
-                if (exists) {
-                    return of(res.sendFile(imageName, { root: './images' }));
-                } else {
-                    this.logger.error(`Image introuvable: ${imageName}`);
-                    throw new BadRequestException('Image non trouvée.');
-                }
-            }),
-            catchError((err) => {
-                this.logger.error(`Erreur lors de l\'envoi de l'image: ${err.message}`);
-                throw new BadRequestException('Erreur lors de l\'envoi de l\'image.');
-            })
-        );
+        if (existsSync(imagePath)) {
+            res.sendFile(imageName, { root: './images' });
+        } else {
+            this.logger.error(`Image introuvable: ${imageName}`);
+            throw new BadRequestException('Image non trouvée.');
+        }
     }
 
     @UseGuards(JwtGuard)
     @Get('image-name')
-    findUserImageName(@Request() req, @Res() res): Observable<{ imageName: string }> {
+    findUserImageName(@Request() req): Observable<{ imageName: string }> {
         const userId = req.user.id;
         return this.userService.findImageNameByUserId(userId).pipe(
             switchMap((imageName: string) => {
@@ -87,19 +73,5 @@ export class UserController {
                 throw new BadRequestException('Erreur lors de la récupération du nom de l\'image.');
             })
         );
-    }
-
-    private checkFileExists(filePath: string): Observable<boolean> {
-        return new Observable<boolean>((observer) => {
-            fs.access(filePath)
-                .then(() => {
-                    observer.next(true);  // Le fichier existe
-                    observer.complete();
-                })
-                .catch(() => {
-                    observer.next(false);  // Le fichier n'existe pas
-                    observer.complete();
-                });
-        });
     }
 }
