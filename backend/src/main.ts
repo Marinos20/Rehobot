918c8b35be3ceacaad7cadcbdbd23b798577a7c5
Marinos <marinos.agounkpe@epitech.eu>
2025-02-17 13:12:03 +0100

    gestion des log avec morgan

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 import { MulterExceptionFilter } from './filters/MulterExceptionFilter';
 import { AllExceptionsFilter } from './core/all-exceptions.filter';
 
+import * as fs from 'fs';
+import * as morgan from 'morgan';
+import { stream } from 'file-type';
+
+//envie de suivre les log avec morgan
+const logStream = fs.createWriteStream('omertaa.log' , {
+    flags: 'a', // append
+})
+
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
@@ -11,6 +20,9 @@ async function bootstrap() {
 
     app.enableCors();
     app.setGlobalPrefix('omertaa');
+    //affichage des log dans le terminal pour raison de sécurité j'ai commenté
+    // app.use(morgan('dev'));
+    app.use(morgan('combined', { stream : logStream}))
 
     // Applique les filtres globalement
     app.useGlobalFilters(new MulterExceptionFilter(), new AllExceptionsFilter());
